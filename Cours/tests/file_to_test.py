2f05aaa8bb8ee7cb8445f949c5d1fe7c9ed8608a
charles-42 <71760090+charles-42@users.noreply.github.com>
2022-10-30 11:24:30 +0100

    add tests

---
+++ Cours/tests/file_to_test.py
@@ -0,0 +1,45 @@
+from random import uniform
+
+
+# Test introduction:
+
+def add(a,b):
+    return a+b
+
+
+def divide(a,b):
+    return a/b
+
+def add_integer(a,b):
+    if isinstance(a,int) and isinstance(b,int):    
+        return a+b
+    else:
+        raise(TypeError("Parameters should be integers"))
+
+def alea_uniform(a,b):
+    return uniform(a,b)
+
+# Adavance test:
+
+def input_function():
+    return input("Que voulez vous renvoyer?")
+
+def create_file(file_path,content):
+    with open(file_path, "w") as fichier:
+        fichier.write(content)
+
+def print_hello():
+    print("hello")
+
+
+def several_input_function():
+    count = 1
+    my_input = input("Qu'avez-vous à déclarer?")
+    while my_input != "Rien Monsieur":
+        my_input = input("Qu'avez-vous à déclarer?")
+        count+=1
+    return count
+    
+def randomize():
+    return uniform(0,1)
+
